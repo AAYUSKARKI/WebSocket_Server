dadeda153fd0b6a38d0127f90788a583887cd549
AAYUS KARKI <alexkarki2060@gmail.com>
2024-10-27 18:16:31 +0545

    websocket server created!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("aayus")
+import express, { Request, Response } from "express";
+import http from "http";
+import WebSocket  from "ws";
+
+const app = express();
+
+// create a http server
+const server = http.createServer(app);
+
+// create a websocket server
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+    console.log("A new client connected");
+    ws.send(JSON.stringify({
+        message: "welome new client"
+    }));
+    ws.on("message", (data) => {
+        const message = JSON.parse(data.toString());
+        console.log(message);
+    })
+})
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("hello");
+})
+
+server.listen(3000, () => {
+    console.log("server started");
+})
